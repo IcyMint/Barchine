a0fc50e729e3bb2d340daba9a58e209668b9f531
Diego Bonilla <diegobonilla004@gmail.com>
2020-02-23 21:04:45 -0500

    Expanded Ingredient Functionality

--- Ingredient Library.py
+++ Ingredient Library.py
@@ -15,7 +15,7 @@ class BaseTypes():
         'Campari',
         'Cointreau',
         'Drambuie',
-        'Gin','Gin'),
+        'Gin',
         'Jagermeister',
         'Liqeueur',
         'Rum',
@@ -40,19 +40,35 @@ class Ingredient:
 
     #String representation for storage
     def __str__(self):
-        
+        return self.name+','+self.base+','+self.family+','+self.startVol+','+self.endVol+','+self.active
 
 #Create an ingredient
-def createIngredient():
+def createIngredient(name,base,family,startVol, endVol, active):
+    ingredient = Ingredient(name, base, family, startVol, startVol, active)
+    addIngredient(ingredient)
 
 #Add ingredient to library
-def addIngredient():
+def addIngredient(new_ingredient):
+    library.append(new_ingredient)
 
 #Commit library to storage
 def storeLibrary():
+    file = open("IngredientRepo.txt","w")
+    for ingredient in library:
+        file.write(ingredient.__str__)
+    file.close()
 
 #Restore library from storage
 def restoreLibrary():
+    file.open("IngredientRepo.txt","w")
+    for line in file:
+        elements = line.split(',')
+        createIngredient(elements[0],elements[1],elements[2],elements[3],elements[4],elements[5])
     
 #Delete ingredient from library
-def deleteIngredient():
+def deleteIngredient(name):
+    marked = null
+    for element in library:
+        if element.name == name:
+            marked = element
+    library.remove(element)
